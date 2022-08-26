4d3a7c829f375960682dd25edd39bb229d9e8f96
Otávio Azevedo <otavioad1997@gmail.com>
2022-08-26 19:49:40 -0300

    Requisito 4

--- src/login/login.js
+++ src/login/login.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const generateToken = require('../utils/generateToken');
+const emailValidation = require('../middleware/email');
+const passwordValidation = require('../middleware/password');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    // const { email, password } = req.body;
+router.post('/', emailValidation, passwordValidation, (req, res) => {
     const tokenValue = generateToken();
     res.status(200).json({ token: tokenValue });
 });
---
+++ src/middleware/email.js
@@ -0,0 +1,13 @@
+const Validation = (req, res, next) => {
+    const { email } = req.body;
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!email) {
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    }
+    if (!email.match(regex)) {
+       return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    }
+    next();
+};
+
+module.exports = Validation;
